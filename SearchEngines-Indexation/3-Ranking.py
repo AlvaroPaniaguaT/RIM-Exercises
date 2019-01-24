a60f6124126790eec00269b34493cc5241ef7497
AlvaroPaniaguaT <a.paniaguat@alumnos.urjc.es>
2019-01-24 18:39:11 +0100

    Updating 3-Ranking exercise, closes #2

--- SearchEngines-Indexation/3-Ranking.py
+++ SearchEngines-Indexation/3-Ranking.py
@@ -4,20 +4,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class RankingIndex():
-    def __init__(self, files, files_content):
+    def __init__(self, files):
         self.filenames = files
-        self.files_content = files_content
-        self.process_files()
+        self.files_content = [ open(filename, 'r').read() for filename in filenames] # Read content of each file
+        self.tfidf = TfidfVectorizer(analyzer='word', strip_accents="unicode")
     
-    def process_files(self):
-        tfidf = TfidfVectorizer(analyzer='word', strip_accents="unicode")
-        tfidf_matrix = tfidf.fit_transform(self.files_content)
+    def process_files(self, query):
+        self.files_content.append(query)
+        tfidf_matrix = self.tfidf.fit_transform(self.files_content)
         similarity = cosine_similarity(tfidf_matrix) # Calculates distance between documents
-        self.ranking = self.order_texts(similarity)
-    
-    def order_texts(self, similarity):
+        return similarity
+
+class RankingQuery():
+    def __init__(self, query, ranking_index):
+        self.ranking_index = ranking_index
+        self.query = query
+        self.similarity = self.ranking_index.process_files(query)
+
+    def get_most_similar_texts(self):
         # Se obtiene la fila completa.
-        sim_rows = similarity[0:, -1]
+        sim_rows = self.similarity[0:, -1]
         index_ordering = {}
         for i in range(0, len(sim_rows)):
             # Se guarda cada valor para no perderlo al ordenar
@@ -28,11 +34,8 @@ class RankingIndex():
         for col in sim_cols_ordered:
             # Se recuperan los nombres de los ficheros originales salvo el propio (tiene similaridad 1 consigo mismo).
             if col < 1:
-                files.append(self.filenames[index_ordering[col]])
+                files.append(self.ranking_index.filenames[index_ordering[col]])
         return files
-
-    def get_ranking(self):
-        return self.ranking
 
 if __name__  == "__main__":
     terminate = False
@@ -41,10 +44,8 @@ if __name__  == "__main__":
         query = input("Enter your query --> ")
         if(query != "exit()"):
             filenames = ['../corpus/pg135.txt', '../corpus/pg76.txt', '../corpus/pg5200.txt'] # File list
-            files_content = [ open(filename, 'r').read() for filename in filenames] # Read content of each file
-            files_content.append(query) # Add query to the file content list
-
-            ranking = RankingIndex(filenames, files_content)
-            print(ranking.get_ranking())
+            ranking = RankingIndex(filenames)
+            r_query = RankingQuery(query, ranking)
+            print(r_query.get_most_similar_texts())
         else:
             terminate = True
